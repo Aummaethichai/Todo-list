c048b00b7e91fec186157845cafd079184ccca92
Maethichai Kobkam <maethichai.ko@inet.co.th>
2024-11-04 10:29:47 +0700

    update css and main function

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,166 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
+import {
+  Box,
+  TextField,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+  List,
+  ListItem,
+  ListItemText,
+  Divider,
+  IconButton,
+} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+interface taskType {
+  id: number;
+  text: string;
+  completed: boolean;
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [tasks, setTasks] = useState<taskType[]>([]);
+  const [text, setText] = useState("");
+  const [text_field, set_show_field] = useState(false);
+  const [confirm, set_confirm] = useState(false)
+  function addTask(text: string) {
+    const newTask = {
+      id: Date.now(),
+      text,
+      completed: false,
+    };
+    setTasks([...tasks, newTask]);
+    setText("");
+    set_show_field(false);
+  }
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  function deleteTask(id:number) {
+    setTasks(tasks.filter(task => task.id !== id));
+    }
+
+  const handleClickOpenTextField = () => {
+    set_show_field(true);
+  };
+
+  const handleClickCloseTextField = () => {
+    set_show_field(false);
+  };
+
+  const handleClickDelete = () => {
+    set_confirm(true);
+  };
+
+  return (
+    <div className="flex justify-center items-center h-screen flex-col gap-8">
+      <div className="flex justify-center border-b-2 border-red-300">
+        <h1 className="">TODO-LIST</h1>
+      </div>
+      <div className="w-1/3 h-1/3 p-3 flex-col">
+        <div className="flex justify-between pb-3">
+          <div className="">
+            <button
+              className="border-2 bg-green-200 border-green-500 rounded-lg p-2 hover:border-green-800 hover:scale-125 active:bg-green-300 transition delay-150 duration-300 ease-in-out"
+              onClick={handleClickOpenTextField}
+            >
+              Add task
+            </button>
+            <Dialog
+              open={text_field}
+              onClose={handleClickCloseTextField}
+              aria-labelledby="alert-dialog-title"
+              aria-describedby="alert-dialog-description"
+            >
+              <DialogTitle id="alert-dialog-title">
+                {"รายการที่ต้องการบันทึก"}
+              </DialogTitle>
+              <DialogContent>
+                <Box
+                  component="form"
+                  sx={{ "& > :not(style)": { m: 1, width: "25ch" } }}
+                  noValidate
+                  autoComplete="off"
+                >
+                  <TextField
+                    id="outlined-basic"
+                    variant="outlined"
+                    value={text}
+                    onChange={(e) => setText(e.target.value)}
+                  />
+                </Box>
+              </DialogContent>
+              <DialogActions>
+                <Button className="bg-red-500 text-white" onClick={handleClickCloseTextField}>Disagree</Button>
+                <Button
+                  className="bg-green-500 text-white"
+                  onClick={() => {
+                    addTask(text);
+                  }}
+                >
+                  Agree
+                </Button>
+              </DialogActions>
+            </Dialog>
+          </div>
+          {/* <div className="">
+            <button className="border-4 bg-blue-200 border-blue-500 rounded-lg p-2 hover:border-blue-800 active:bg-blue-300">
+              TEST
+            </button>
+          </div> */}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="flex justify-center items-center p-2 border-2 border-gray-600 rounded-xl bg-slate-200">
+          <List
+            sx={{
+              width: "100%",
+              maxWidth: 360,
+              bgcolor: "",
+              position: "relative",
+              overflow: "auto",
+              maxHeight: 300,
+              height: 300,
+              "& ul": { padding: 0 },
+            }}
+            subheader={<li />}
+          >
+            {tasks.map((item, index) => (
+              <li key={`section-${index}`}>
+                <ul>
+                  <ListItem key={index}>
+                    <ListItemText primary={item.text} />
+                    <IconButton aria-label="delete" onClick={()=> deleteTask(item.id)}>
+                      <DeleteIcon />
+                    </IconButton>
+                  </ListItem>
+                  <Dialog
+              open={confirm}
+              onClose={handleClickDelete}
+              aria-labelledby="alert-dialog-title"
+              aria-describedby="alert-dialog-description"
+            >
+              <DialogTitle id="alert-dialog-title">
+                {"Confirm Delete"}
+              </DialogTitle>
+              <DialogContent>
+              <Button onClick={()=>set_confirm(false)}>Disagree</Button>
+                <Button
+                  onClick={() => {
+                    addTask(text);
+                  }}
+                  className="bg-green-500"
+                >
+                  Agree
+                </Button>
+              </DialogContent>
+            </Dialog>
+                  <Divider />
+                </ul>
+              </li>
+            ))}
+          </List>
+        </div>
+      </div>
     </div>
   );
 }
